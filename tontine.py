bf923daf1887987417994aa3b0a98422281bb545
tahasene <mtahasene@gmail.com>
2025-06-21 23:26:59 +0000

    Add files via upload

---
+++ tontine.py
@@ -0,0 +1,15 @@
+membres =[
+    {"id":1, "nom":"alice","tel":782240527,"actif": true ,"cotisations": []}
+    ]
+
+def ajouter_membre(nom, tel):
+
+
+def desactiver_membre(membre_id):
+
+
+def enregistrer_cotisation(membre_id, montant, date, cycle):
+
+
+
+def verifier_retards(membre_id):
